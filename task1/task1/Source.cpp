b38128c7ff29c32317869acc7c4fe6abe8c5a775
Alexander <sasha1_1997@mail.ru>
2017-10-13 00:18:14 +0300

    create first version

--- task1/task1/Source.cpp
+++ task1/task1/Source.cpp
@@ -1,13 +1,45 @@
 #include <mpi.h>
 #include<iostream>
-using namespace std;
+using std::cin;
+using std::cout;
+#define tag 1
 int main(int argc, char **argv)
 {
-	int rank, size;
+	int rank, size, numPoint = 10;
+	double ans = 0;
+
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	cout << "The number of processes: " << size << " my number is " << rank << endl;
+
+	int delta = (numPoint % size) - rank;
+	if (delta > 0) 
+		delta = 1;
+	else delta = 0;
+	
+	double sum = 0;
+	for (int i = 0; i < delta + numPoint / size; i++)
+		sum += 1;
+	//sum /= numPoint;
+
+	cout << numPoint / size + delta<< ' ' <<sum<< " i count \n";
+	if (rank != 0)
+		MPI_Send(&sum, 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+	else
+	{
+		ans += sum;
+		double tmp;
+		MPI_Status status;
+		for (int i = 0; i < size - 1; i++)
+		{
+			MPI_Recv(&tmp, 1, MPI_DOUBLE, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
+			cout <<'!'<<tmp<<'!';
+			ans += tmp;
+			cout << ans << '!';
+		}
+		cout << "ans = " << ans <<'\n';
+	}
+//	cout << "The number of processes: " << size << " my number is " << rank << '\n';
 	MPI_Finalize();
 	return 0;
 }
