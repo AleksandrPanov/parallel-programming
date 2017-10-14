7c1cd1a3bc01f59a7c374e96262c6717877caed4
Alexander <Sasha1_1997@mail.ru>
2017-10-14 18:55:25 +0400

    release

--- task1/task1/Source.cpp
+++ task1/task1/Source.cpp
@@ -1,28 +1,45 @@
 #include <mpi.h>
 #include<iostream>
+#include <random>
+#include <ctime>
+#include <cmath>
+
+#define tag 1
+double const pi = 3.14159265358979323846;
 using std::cin;
 using std::cout;
-#define tag 1
+inline int getDelta(int numPoint, int size, int rank)
+{
+	int delta = (numPoint % size) - rank;
+	return delta > 0;
+}
+using CalculateFunc = double(double x);
+CalculateFunc *funcAr[2] = {sin, cos};
 int main(int argc, char **argv)
 {
-	int rank, size, numPoint = 10;
-	double ans = 0;
+	int rank, size, numPoint = 1000000, numFunc = 0;
+	double ans = 0, a = -10*pi, b = 10*pi;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	int delta = (numPoint % size) - rank;
-	if (delta > 0) 
-		delta = 1;
-	else delta = 0;
-	
+	std::mt19937_64 gen(time(0));
+	std::uniform_real_distribution<> genFromAtoB(a + rank*(b - a)/(double)size, a + (rank+1)*(b - a) / (double)size);
+	//std::uniform_real_distribution<> genFromAtoB(a,b);
+
+
+	int delta = getDelta(numPoint, size, rank);	
+
 	double sum = 0;
 	for (int i = 0; i < delta + numPoint / size; i++)
-		sum += 1;
-	//sum /= numPoint;
+	{
+		double x = genFromAtoB(gen);
+		sum += funcAr[numFunc](x);
+	}
+	sum /= numPoint;
 
-	cout << numPoint / size + delta<< ' ' <<sum<< " i count \n";
+	//cout << numPoint / size + delta<< ' ' <<sum<< " count sum\n";
 	if (rank != 0)
 		MPI_Send(&sum, 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
 	else
@@ -34,6 +51,8 @@ int main(int argc, char **argv)
 		{
 			MPI_Recv(&tmp, 1, MPI_DOUBLE, MPI_ANY_SOURCE, tag, MPI_COMM_WORLD, &status);
 			ans += tmp;
+		}
+		ans *= (b - a);
 		cout << "ans = " << ans <<'\n';
 	}
 //	cout << "The number of processes: " << size << " my number is " << rank << '\n';
