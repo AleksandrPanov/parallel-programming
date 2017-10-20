d6773be2399822adff588cf12ce13e0db1e0b9c5
Alexander <Sasha1_1997@mail.ru>
2017-10-20 12:38:11 +0400

    add input

--- task1/task1/Source.cpp
+++ task1/task1/Source.cpp
@@ -22,14 +22,29 @@ CalculateFunc *funcAr[3] = {sin, cos, fx};
 int main(int argc, char **argv)
 {
 	int rank, size, numPoint = 100000000, numFunc = 0;
-	double ans = 0, a = -10*pi, b = 10*pi, startTime;
+	double ans = 0, a = 0, b = pi, startTime;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::mt19937_64 const gen(time(0));
-	std::uniform_real_distribution<> const genFromAtoB(a + rank*(b - a)/(double)size, a + (rank+1)*(b - a) / (double)size);
+	std::mt19937_64 gen(time(0));
+	std::uniform_real_distribution<> genFromAtoB(a + rank*(b - a)/(double)size, a + (rank+1)*(b - a) / (double)size);
+	char c;
+	if (rank == 0)
+	{
+		cout << "Do you want set a, b, func and num of point? Y N\n";
+		cin >> c;
+		if (c == 'Y')
+		{
+			cin >> a >> b >> numFunc >> numPoint;
+		}
+	}
+
+	MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&numFunc, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&numPoint, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	startTime = MPI_Wtime();
 	int delta = getDelta(numPoint, size, rank);	
--- task1_sequential/ConsoleApplication1/Source.cpp
+++ task1_sequential/ConsoleApplication1/Source.cpp
@@ -15,12 +15,19 @@ CalculateFunc *funcAr[3] = { sin, cos, fx };
 int main(int argc, char **argv)
 {
 	int numPoint = 100000000, numFunc = 0;
-	double a = -10 * pi, b = 10 * pi;
+	double a = 0, b = 2 * pi;
 
 	std::mt19937_64 gen(time(0));
 	std::uniform_real_distribution<> genFromAtoB(a, b);
 	
 	double sum = 0, x;
+	char c;
+	cout << "Do you want set a, b, func and num of point? Y N\n";
+	cin >> c;
+	if (c == 'Y')
+	{
+		cin >> a >> b >> numFunc >> numPoint;
+	}
 	auto start_time = std::chrono::steady_clock::now();
 	for (int i = 0; i < numPoint; i++)
 	{
