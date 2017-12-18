c0b5b32042b68469fe9e5f1f93d2b09449ce8afd
Alexander <Sasha1_1997@mail.ru>
2017-12-19 01:30:57 +0300

    added final

--- task3/task3/Source.cpp
+++ task3/task3/Source.cpp
@@ -6,9 +6,13 @@
 
 #define tag 1
 using std::cout;
+func *interference = [](double x)
+{
+	return 0.1*sin(50*x);
+};
 func *f = [](double x)
 {
-	return  sin(x) + cos(x);
+	return  sin(x) + interference(x);
 };
 funcPeriodic *g = [](func f, double x, double middle, double end)
 {
@@ -22,7 +26,7 @@ void MPI_Start(int &argc, char **argv, int &size, int &rank)
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 }
-void FillData(int n, vec &points, double l, double r, func f1, funcPeriodic f2, int rank)
+void FillData(int n, vec &points, double l, double r, func f1, funcPeriodic f2, int rank, int print)
 {
 	points = vec(n);
 	if (rank == 0)
@@ -36,10 +40,13 @@ void FillData(int n, vec &points, double l, double r, func f1, funcPeriodic f2, 
 				points[i] = f2(f1, x, (r-l)/2, r);
 			x += delta;
 		}
-		/*ofstream of;
-		openForWrite(of, "sinX", true);
-		writePoints(n, points.getP(), of);
-		of.close();*/
+		if (print)
+		{
+			ofstream of;
+			openForWrite(of, "original", true);
+			writePoints(n, points.getP(), of);
+			of.close();
+		}
 	}
 }
 void PostAllData(int n, vec &points)
@@ -51,41 +58,41 @@ void calculate(FourierFunction &four, double x, double &res, double &allRes)
 	res = four.calculate(x);
 	MPI_Reduce(&res, &allRes, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 }
-void setPoint(FourierFunction &f)
-{
-
-}
 int main(int argc, char **argv)
 {
-	int rank, size, n = 11, m;
+	int rank, size, n = 20001, m, print = 0;
 	vec points;
 	FourierFunction fourierFunction;
-	double lr[2] = { 0, 2 * pi }, startTime, endTime, res = 0, x = pi/4, allRes = 0;
+	double lr[2] = { 0, 2 * pi }, startTime, endTime, res = 0, x = pi/2, allRes = 0;
 	if (argc > 1)
 	{
 		n = atoi(argv[1]);
+		if (argc > 2)
+		{
+			print = atoi(argv[2]);
+		}
 	}
-	m = n/2 + 1;
+	m = n / 2 + 1;
 
 	MPI_Start(argc, argv, size, rank);
-	FillData(n, points, lr[0], lr[1], f, 0, rank);
+	FillData(n, points, lr[0], lr[1], f, 0, rank, print);
 	PostAllData(n, points);
 	if (rank == 0)
 		startTime = MPI_Wtime();
 	fourierFunction.setCoeff(n, fourierFunction.getM(rank, size, m), fourierFunction.getStartM(rank, size, m), lr[0], lr[1], points.getP());
-	//res = fourierFunction.calculate(x);
-	//MPI_Reduce(&res, &allRes, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	calculate(fourierFunction, x, res, allRes);
 	if (rank == 0)
 	{
 		endTime = MPI_Wtime() - startTime;
 		cout << "time =  " <<endTime << " res in point x = "<<x<<" is "<<allRes<<'\n';
-		/*
-		ofstream of;
-		fourierFunction.setPoints(n, points);
-		openForWrite(of, "sinXFourier", true);
-		writePoints(n, points.getP(), of);
-		*/
+		if (print)
+		{
+			ofstream of;
+			fourierFunction.setPoints(n, points);
+			openForWrite(of, "out", true);
+			writePoints(n, points.getP(), of);
+		}
+		
 	}
 	MPI_Finalize();
 	return 0;
--- task3/task3/read_write.h
+++ task3/task3/read_write.h
@@ -6,7 +6,7 @@ using std::string;
 void writePoints(int n, double *ar, std::ofstream &of)
 {
 	for (int i = 0; i < n; i++)
-		of << ar[i] << ';';
+		of << ar[i] << " ;";
 }
 void openForWrite(std::ofstream &f, string name, bool createNewFile)
 {
